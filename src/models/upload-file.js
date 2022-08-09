6a0eb024a1d49f5ef2dce5f01bb054f6abdfc544
Ruben Gianelli <rubengianelli8@gmail.com>
2022-08-09 15:26:25 -0300

    s3 agregado

--- src/models/upload-file.js
+++ src/models/upload-file.js
@@ -1,13 +1,14 @@
 import { prisma } from "@/prisma/client";
 import { error } from "./error";
-
+import { uploadFile } from "@/utils/s3";
 export const uploadFile = {
   async addDemo(_parent, { data, file }, _context) {
     try {
+      const fileName = await uploadFile.upload(file, "/file");
       return prisma.demo.create({
         data: {
           ...data,
-          file,
+          file_name: fileName,
         },
       });
     } catch (e) {
