b44c1fdad2ff695ab6cb2840f713bd919a91681d
Ruben Gianelli <rubengianelli8@gmail.com>
2022-08-11 11:09:13 -0300

    fix next.config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,24 @@
+const withPlugins = require("next-compose-plugins");
 const nextTranslate = require("next-translate");
-
-module.exports = nextTranslate();
+// https://github.com/vercel/next.js/discussions/15341
+const withTM = require("next-transpile-modules")([
+  "swagger-ui-react",
+  "swagger-client",
+  "react-syntax-highlighter",
+]);
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ["platform-lookaside.fbsbx.com"],
+  },
+  webpack: (config, { isServer, webpack }) => {
+    if (isServer) {
+      config.externals.push("_http_common");
+    }
+    return config;
+  },
+};
+module.exports = withPlugins(
+  [[withTM, { transpileModules: [] }], nextTranslate],
+  nextConfig
+);
